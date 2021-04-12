58aa6f77f69baafaf6b808a87ed173a0b39c00c0
lapc18 <lapc18@outlook.com>
2021-04-12 12:36:42 -0400

    feat(enat): add navbar component(ui)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    CardsComponent
+    CardsComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule
