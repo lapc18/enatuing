fe7b3da8d1805318264e6d3cb6a601e73d8426d0
lapc18 <lapc18@outlook.com>
2021-04-12 15:27:18 -0400

    ref(enat): exporting shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
@@ -10,10 +11,17 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   declarations: [
     FooterComponent,
     CardsComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
+  ], 
+  exports: [
+    FooterComponent,
+    CardsComponent,
+    NavbarComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
