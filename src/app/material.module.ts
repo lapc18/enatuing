e540b44a933b1df58f6ad95f6d893a42534a048b
lapc18 <lapc18@outlook.com>
2021-04-17 23:00:32 -0400

    feat(enat): add angular material(ui)

---
+++ src/app/material.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        MatSliderModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatSelectModule,
+        MatCardModule,
+        MatDialogModule,
+        MatInputModule,
+        MatRadioModule,
+        MatFormFieldModule,
+        MatCheckboxModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatTooltipModule,
+        MatProgressSpinnerModule
+    ],
+    exports: [
+        MatSliderModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatSelectModule,
+        MatCardModule,
+        MatDialogModule,
+        MatInputModule,
+        MatRadioModule,
+        MatFormFieldModule,
+        MatCheckboxModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatTooltipModule,
+        MatProgressSpinnerModule
+    ]
+})
+export class MaterialModule { }
