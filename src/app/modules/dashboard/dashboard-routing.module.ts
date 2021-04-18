fb46dccef17055e125e122eadfce639769494164
lapc18 <lapc18@outlook.com>
2021-04-18 01:32:08 -0400

    feat(enat): add organization module

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -3,15 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'contacts', loadChildren: () => import('../contacts/contacts.module').then(m => m.ContactsModule) }, 
-  { path: 'users', loadChildren: () => import('../users/users.module').then(m => m.UsersModule) }, 
-  { path: 'normatives', loadChildren: () => import('../normatives/normatives.module').then(m => m.NormativesModule) }, 
-  { path: 'auth', loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule) }, 
-  { path: 'metrics', loadChildren: () => import('../metrics/metrics.module').then(m => m.MetricsModule) }, 
-  { path: 'certifications', loadChildren: () => import('../certifications/certifications.module').then(m => m.CertificationsModule) }, 
-  { path: 'queue', loadChildren: () => import('../queue/queue.module').then(m => m.QueueModule) },
-  { path: 'dashboard', loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule) }
+  { path: '', component: DashboardComponent, children: [
+      { path: 'contacts', loadChildren: () => import('../contacts/contacts.module').then(m => m.ContactsModule) }, 
+      { path: 'users', loadChildren: () => import('../users/users.module').then(m => m.UsersModule) }, 
+      { path: 'normatives', loadChildren: () => import('../normatives/normatives.module').then(m => m.NormativesModule) }, 
+      { path: 'auth', loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule) }, 
+      { path: 'metrics', loadChildren: () => import('../metrics/metrics.module').then(m => m.MetricsModule) }, 
+      { path: 'certifications', loadChildren: () => import('../certifications/certifications.module').then(m => m.CertificationsModule) }, 
+      { path: 'queue', loadChildren: () => import('../queue/queue.module').then(m => m.QueueModule) },
+      { path: 'organizations', loadChildren: () => import('../organization/organization.module').then(m => m.OrganizationModule) },
+      { path: 'dashboard', loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule) }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MaterialModule } from 'src/app/material.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    SharedModule
+    SharedModule,
+    MaterialModule
   ]
 })
 export class DashboardModule { }
