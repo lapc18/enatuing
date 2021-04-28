62c54652fd27e7ebd50832e0f4e65993f8555008
lapc18 <lapc18@outlook.com>
2021-04-28 14:08:25 -0400

    feat(enat): create add event into header(shared)

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -14,6 +14,7 @@ export class HeaderComponent implements OnInit {
 
   @Output() onExport: EventEmitter<FileType> = new EventEmitter();
   @Output() onFilter: EventEmitter<string> = new EventEmitter();
+  @Output() onAddItem: EventEmitter<any> = new EventEmitter();
 
 
   constructor() { }
