85be1341df5b76decbc422dbd93b66d0fafecf30
lapc18 <lapc18@outlook.com>
2021-05-31 23:23:24 -0400

    feat(enat): add api service (certifications)

---
+++ src/app/core/services/certifications.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { CertificationEntity } from "../domain/certifications/certifications.entity";
+import { Certification } from "../domain/certifications/certifications.models";
+import { AbstractEnatService } from "../models/base-service.abstract";
+import { columnSettings } from "../models/enat.models";
+import { baseServiceOptions } from "../models/service-properties.model";
+
+
+@Injectable()
+export class CertificationsService extends AbstractEnatService<Certification, CertificationEntity> {
+
+    constructor(
+        public http: HttpClient,
+    ) {
+        super(http, columnSettings.certifications, baseServiceOptions.certifications);
+    }
+
+}
